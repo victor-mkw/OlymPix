ef23fcdad5625f4aedad8d2cbe24a0eb7cb288e5
Mankowski Victor <mankowski.victor@gmail.com>
2024-10-16 18:55:23 +0200

    Ajout de données dans le fichier INSERT + nouvel EXPORT

--- Code SQL/INSERT_TABLE_OLYMPIX_VICTOR_MANKOWSKI.sql
+++ Code SQL/INSERT_TABLE_OLYMPIX_VICTOR_MANKOWSKI.sql
@@ -1,5 +1,8 @@
 INSERT INTO t_compte_cpt VALUES('organisateur', SHA2(CONCAT('OoL56T%','org24*PMYLO'), 512),'A');
 INSERT INTO t_compte_cpt VALUES('rui.duarte@gmail.com', SHA2(CONCAT('OoL65T%', 'OrgN2#CnC29'), 512), 'A');
+INSERT INTO t_compte_cpt VALUES('michel.blanc@gmail.com', SHA2(CONCAT('OoL65T%', 'OrgN3#CnC75'), 512), 'A');
+INSERT INTO t_compte_cpt VALUES('anna.guillou@gmail.com', SHA2(CONCAT('OoL65T%', 'OrgN4#CnC92'), 512), 'A');
+INSERT INTO t_compte_cpt VALUES('charles.carrefour@gmail.com', SHA2(CONCAT('OoL65T%', 'OrgN5#CnC45'), 512), 'A');
 INSERT INTO t_compte_cpt VALUES('victor.mankowski@gmail.com', SHA2(CONCAT('OoL56T%','MdP2LoGin!'), 512),'A');
 INSERT INTO t_compte_cpt VALUES('chleo.lamarre@gmail.com', SHA2(CONCAT('OoL56T%','COnExION95%'), 512),'A');
 INSERT INTO t_compte_cpt VALUES('enzo.gp@gmail.com', SHA2(CONCAT('OoL56T%','LoGALaPplI@29'), 512), 'A');
@@ -8,6 +11,9 @@ INSERT INTO t_compte_cpt VALUES('progamedev@gmail.com', SHA2(CONCAT('OoL56T%','J
 
 INSERT INTO t_administrateur_adm VALUES('organisateur', NULL, 'organisateur');
 INSERT INTO t_administrateur_adm VALUES('rui.duarte@gmail.com', 'Rui', 'Duarte');
+INSERT INTO t_administrateur_adm VALUES('michel.blanc@gmail.com', 'Michel', 'Blanc');
+INSERT INTO t_administrateur_adm VALUES('anna.guillou@gmail.com', 'Anna', 'Guillou');
+INSERT INTO t_administrateur_adm VALUES('charles.carrefour@gmail.com', 'Charles', 'Carrefour');
 
 INSERT INTO t_jury_jry VALUES('victor.mankowski@gmail.com', 'Mankowski', 'Victor', 'Game Developer Senior', 'Je commence à apprendre le developpement de jeux vidéos en 2023...', NULL);
 INSERT INTO t_jury_jry VALUES('chleo.lamarre@gmail.com', 'Lamarre', 'Chleo', 'Experte en psychologie', 'Diplomée d\'un Master en Psychologie Clinique...', NULL);
@@ -17,9 +23,9 @@ INSERT INTO t_jury_jry VALUES('progamedev@gmail.com', NULL, 'Pro Game Dev', 'Inf
 
 INSERT INTO t_concours_cnc VALUES(NULL, "Godot Jam #1", "Première JAM sur le moteur Godot; Le thème: Fragment", '2024-06-01', 7, 15, 2, 'organisateur');
 INSERT INTO t_concours_cnc VALUES(NULL, "Godot Jam #2", "Seconde JAM sur le moteur Godot; Le thème: Nature", '2024-10-01', 7, 15, 7, 'organisateur');
-INSERT INTO t_concours_cnc VALUES(NULL, "Unity Jam #1", "Première JAM sur le moteur Unity; Le thème: Grandeur", '2024-10-01', 15, 7, 7, 'organisateur');
-INSERT INTO t_concours_cnc VALUES(NULL, "Unreal Jam #1", "Première JAM sur le moteur Unreal; Le thème: Populaire", '2024-10-01', 5, 5, 15, 'organisateur');
-INSERT INTO t_concours_cnc VALUES(NULL, "Unity Jam #2", "Seconde JAM sur le moteur Unity; Le thème: Vitesse", '2024-11-01', 15, 7, 7, 'organisateur');
+INSERT INTO t_concours_cnc VALUES(NULL, "Unity Jam #1", "Première JAM sur le moteur Unity; Le thème: Grandeur", '2024-10-01', 15, 7, 7, 'rui.duarte@gmail.com');
+INSERT INTO t_concours_cnc VALUES(NULL, "Unreal Jam #1", "Première JAM sur le moteur Unreal; Le thème: Populaire", '2024-10-01', 5, 5, 15, 'rui.duarte@gmail.com');
+INSERT INTO t_concours_cnc VALUES(NULL, "Unity Jam #2", "Seconde JAM sur le moteur Unity; Le thème: Vitesse", '2024-11-01', 15, 7, 7, 'rui.duarte@gmail.com');
 
 INSERT INTO t_categorie_cat VALUE(NULL,"Débutant");
 INSERT INTO t_categorie_cat VALUE(NULL,"Junior");
@@ -59,6 +65,8 @@ INSERT INTO t_fil_fil VALUES(NULL,'Délibération Concours 2', 2);
 
 INSERT INTO t_message_msg VALUES(NULL,'J\'ai beaucoup aimé le travail de Jean-Baptiste', 1,'victor.mankowski@gmail.com');
 INSERT INTO t_message_msg VALUES(NULL,'Le concept d\'Evan me parait plus poussé dans le thème', 1,'enzo.gp@gmail.com');
+INSERT INTO t_message_msg VALUES(NULL,'J\'espère voir de beaux jeux sur ce concours', 2,'legall.patrick@gmail.com');
+INSERT INTO t_message_msg VALUES(NULL,'Aucun jeu n\'égalera les miens', 2,'progamedev@gmail.com');
 
 INSERT INTO t_notation_nte VALUES(4,'enzo.gp@gmail.com',3); 
 INSERT INTO t_notation_nte VALUES(3,'enzo.gp@gmail.com',2);
@@ -71,5 +79,5 @@ INSERT INTO t_notation_nte VALUES(3,'chleo.lamarre@gmail.com',3);
 INSERT INTO t_notation_nte VALUES(2,'chleo.lamarre@gmail.com',2);
 
 INSERT INTO t_document_doc VALUES(NULL,'Jeu de Martin', '/emplacement/2024-06-02jeu.exe', 'Executable Windows', 2);
-INSERT INTO t_document_doc VALUES(NULL,'Jeu de Jean-Baptiste', '/emplacement/2024-06-06godotjamgame.exe', 'Executable Windows', 2);
+INSERT INTO t_document_doc VALUES(NULL,'Jeu de Jean-Baptiste', '/emplacement/2024-06-06godotjamgame.exe', 'Executable Windows', 1);
 INSERT INTO t_document_doc VALUES(NULL,'Jeu de Federique', '/emplacement/2024-06-03monjeu.exe', 'Executable Windows', 4);
